fc3cf14aef5cff97909c204091c9e53766c9120f
Briseida Montiel <brizism@gmail.com>
2017-09-12 23:05:48 -0400

    add routes in app.js and add appointment/appointment_form components

--- alejandra_salon/client/src/App.js
+++ alejandra_salon/client/src/App.js
@@ -4,12 +4,24 @@ import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import { Redirect } from 'react-router';
 
+import Home from './components/Home';
+import About from './components/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import axios from 'axios';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Salon</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="main">
+            <Route exact path="/" component={Home} />
+            <Route exact path="about" component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
