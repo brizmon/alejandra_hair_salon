a94f13a05f499dcf815f6c1e2cda0ad93b1bc8ae
Briseida Montiel <brizism@gmail.com>
2017-09-15 19:53:57 -0400

    import moment.js & add moment to Appointment component

--- alejandra_salon/client/src/components/Appointment.jsx
+++ alejandra_salon/client/src/components/Appointment.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import moment from 'moment';
 
 const Appointment = (props) => {
     return (
         <div className="appointment-container">
             <h1>{props.appointment.service}</h1>
-            <h1>{props.appointment.appt_time}</h1>
+            <h1>{moment(props.appointment.appt_time).format('MMMM DD YYYY, h:mm:ss a')}</h1>
             <h1>{props.appointment.hairstylist}</h1>
         </div>
     )
