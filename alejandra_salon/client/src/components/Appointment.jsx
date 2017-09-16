a28721ed3abda293ff122b475b47dcb660df460c
Briseida Montiel <brizism@gmail.com>
2017-09-16 14:31:12 -0400

    change DD to Do in appointment.js

--- alejandra_salon/client/src/components/Appointment.jsx
+++ alejandra_salon/client/src/components/Appointment.jsx
@@ -6,7 +6,7 @@ const Appointment = (props) => {
     return (
         <div className="appointment-container">
             <h1>{props.appointment.service}</h1>
-            <h1>{moment(props.appointment.appt_time).format('MMMM DD YYYY, h:mm:ss a')}</h1>
+            <h1>{moment(props.appointment.appt_time).format('MMMM Do YYYY, h:mm:ss a')}</h1>
             <h1>{props.appointment.hairstylist}</h1>
         </div>
     )
