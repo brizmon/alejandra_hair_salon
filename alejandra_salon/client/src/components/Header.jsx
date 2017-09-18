39188a6329a48c73446195be8088bcad8532a17d
Briseida Montiel <brizism@gmail.com>
2017-09-18 09:47:07 -0400

    add links to header

--- alejandra_salon/client/src/components/Header.jsx
+++ alejandra_salon/client/src/components/Header.jsx
@@ -3,12 +3,37 @@ import {Link} from 'react-router-dom';
 
 const Header = (props) => {
     return (
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <Link className='navbar-item' Link to="/about">LOGO</Link>
-            </div>
-        </nav>
+        <div>
+            <ul id="dropdown1" className="dropdown-content">
+                <li><Link Link to="/hours-location">Hours & Location</Link></li>
+                <li><Link Link to="/team">Team</Link></li>
+            </ul>
+            <nav>
+                <div className="nav-wrapper black">
+                    <a href="/"><img src="../img/alejandra-logo.svg" alt="Alejandra Unisex" className="logo"></img></a>
+                    <a href="" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+                    <ul className="right hide-on-med-and-down">
+                        <li><Link Link to="/appointments">Appointments</Link></li>
+                        <li><a className="dropdown-button" href="" data-activates="dropdown1">About</a></li>
+                        <li><Link Link to="/services">Services</Link></li>
+                        <li><Link Link to="https://www.instagram.com/alejandraunisex/"><i className="fa fa-instagram fa-lg" aria-hidden="true"></i></Link></li>
+                        <li><Link Link to="https://www.facebook.com/alejandrabeautysalon"><i className="fa fa-facebook fa-lg" aria-hidden="true"></i></Link></li>
+                        <li><Link Link to="https://www.facebook.com/alejandrabeautysalon"><i className="fa fa-pinterest fa-lg" aria-hidden="true"></i></Link></li>
+                    </ul>
+                    <ul className="side-nav" id="mobile-demo">
+                        <li><a href="sass.html">Appointments</a></li>
+                        <li><a href="badges.html">About</a></li>
+                        <li><a href="collapsible.html">Services</a></li>
+                        <li><a href="sass.html"><i className="fa fa-instagram fa-lg" aria-hidden="true"></i></a></li>
+                        <li><a href="sass.html"><i className="fa fa-facebook fa-lg" aria-hidden="true"></i></a></li>
+                        <li><a href="sass.html"><i className="fa fa-pinterest fa-lg" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )
 }
 
 export default Header;
+
+
