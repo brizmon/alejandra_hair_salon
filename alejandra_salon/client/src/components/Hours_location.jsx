5b894421be3863961c48e3b169e71bf666e4a1d2
Briseida Montiel <brizism@gmail.com>
2017-09-18 09:46:25 -0400

    add three new components

---
+++ alejandra_salon/client/src/components/Hours_location.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import {Link} from 'react-router-dom';
+
+const HoursLocation = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Hours & Location</h1>
+        </div>
+    )
+}
+
+
+export default HoursLocation;
---
+++ alejandra_salon/client/src/components/Team.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import {Link} from 'react-router-dom';
+
+const Team = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Team</h1>
+        </div>
+    )
+}
+
+
+export default Team;
