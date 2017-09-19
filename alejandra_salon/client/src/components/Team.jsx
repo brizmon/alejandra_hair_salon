903c0cc32e5845e1f7e994c9065a8aee62f8e4fb
Briseida Montiel <brizism@gmail.com>
2017-09-19 10:09:45 -0400

    add styles to team component

--- alejandra_salon/client/src/components/Team.jsx
+++ alejandra_salon/client/src/components/Team.jsx
@@ -6,6 +6,20 @@ const Team = () => {
         <div>
             <div className="container">
                 <h1 className="center-align">Team</h1>
+                <div className="row">
+                    <div className="col s12 m4 l4">
+                        <div className="card-panel black"><span className="white-text services-hl">ALEJANDRA</span><br></br><hr></hr><span className="white-text"></span>
+                        </div>
+                    </div>
+                    <div className="col s12 m4 l4">
+                        <div className="card-panel black"><span className="white-text services-hl">OTY</span><br></br><hr></hr><span className="white-text"></span>
+                        </div>
+                    </div>
+                    <div className="col s12 m4 l4">
+                        <div className="card-panel black"><span className="white-text services-hl">GLORIA</span><br></br><hr></hr><span className="white-text"></span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
