497d1a4d812be23dbeed152419495f94f872bb38
Briseida Montiel <brizism@gmail.com>
2017-09-18 22:29:21 -0400

    add parallax to custom.js

--- alejandra_salon/client/public/custom.js
+++ alejandra_salon/client/public/custom.js
@@ -1,7 +1,7 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
     $(".dropdown-button").dropdown();
-    $('select').material_select();
+    $('.parallax').parallax();
     
 
 
