4ef2a8711ed1560061b1cb0781736d6d1fbe8e02
Briseida Montiel <brizism@gmail.com>
2017-09-18 22:29:32 -0400

    add links to mobile nav

--- alejandra_salon/client/src/components/Header.jsx
+++ alejandra_salon/client/src/components/Header.jsx
@@ -14,10 +14,9 @@ const Header = (props) => {
             </ul>
             <nav>
                 <div className="nav-wrapper black">
-                    <a href="/"><img src="../img/alejandra-logo.svg" alt="Alejandra Unisex" className="logo"></img></a>
+                    <a href="/"><img src="../img/alejandra-logo.svg" alt="Alejandra Unisex" className="brand-logo"></img></a>
                     <a href="" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
                     <ul className="right hide-on-med-and-down">
-                        {/* <li><Link Link to="">Appointments</Link></li> */}
                         <li><a className="dropdown-button" href="" data-activates="dropdown1">Appointments</a></li>
                         <li><a className="dropdown-button" href="" data-activates="dropdown2">About</a></li>
                         <li><Link Link to="/services">Services</Link></li>
@@ -26,12 +25,14 @@ const Header = (props) => {
                         <li><a href="https://www.facebook.com/alejandrabeautysalon" target="_blank"><i className="fa fa-pinterest fa-lg" aria-hidden="true"></i></a></li>
                     </ul>
                     <ul className="side-nav" id="mobile-demo">
-                        <li><a href="sass.html">Appointments</a></li>
-                        <li><a href="badges.html">About</a></li>
-                        <li><a href="collapsible.html">Services</a></li>
-                        <li><a href="sass.html"><i className="fa fa-instagram fa-lg" aria-hidden="true"></i></a></li>
-                        <li><a href="sass.html"><i className="fa fa-facebook fa-lg" aria-hidden="true"></i></a></li>
-                        <li><a href="sass.html"><i className="fa fa-pinterest fa-lg" aria-hidden="true"></i></a></li>
+                        <li><Link Link to="/book">Book</Link></li>
+                        <li><Link Link to="/appointments">Appointments</Link></li>
+                        <li><Link Link to="/hours-location">Hours & Location</Link></li>
+                        <li><Link Link to="/team">Team</Link></li>
+                        <li><Link Link to="/services">Services</Link></li>
+                        <li><a href="https://www.instagram.com/alejandraunisex/" target="_blank"><i className="fa fa-instagram fa-lg" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.facebook.com/alejandrabeautysalon" target="_blank"><i className="fa fa-facebook fa-lg" aria-hidden="true"></i></a></li>
+                        <li><a href="https://www.facebook.com/alejandrabeautysalon" target="_blank"><i className="fa fa-pinterest fa-lg" aria-hidden="true"></i></a></li>
                     </ul>
                 </div>
             </nav>
